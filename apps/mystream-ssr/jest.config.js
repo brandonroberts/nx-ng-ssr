5a3d534a0bbdba16242518e84fb8112f57cf9197
Brandon Roberts <brandon@nrwl.io>
2021-07-22 13:53:01 -0500

    chore: add ssr example

---
+++ apps/mystream-ssr/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'mystream-ssr',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/mystream-ssr',
+};
---
+++ apps/mystream/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngssr-root',
+  template: '',
+  styles: [''],
+})
+export class AppComponent {
+  title = 'mystream';
+}
